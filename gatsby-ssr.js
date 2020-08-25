4f2a362a2eeaff9cad9bcd33ffcf7be55a213c7d
jiri3 <kotsukotsu@kotsukotsu.work>
2020-08-25 22:25:13 +0900

    [update]GoogleのAdSenseコードを実装する

---
+++ gatsby-ssr.js
@@ -0,0 +1,30 @@
+const React = require("react")
+
+exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+  if (process.env.NODE_ENV !== `production` && process.env.NODE_ENV !== `test`)
+    return null
+  setHeadComponents([...adsbygoogle()])
+}
+
+adsbygoogle = () => {
+  const adsbygoogle = (
+    <script
+      key="adsbygoogle"
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    ></script>
+  )
+  const adsbygoogleConfig = (
+    <script
+      key="adsbygoogle-config"
+      dangerouslySetInnerHTML={{
+        __html: `(adsbygoogle = window.adsbygoogle || []).push(
+            {
+                google_ad_client: "ca-pub-3474413710491680",
+                enable_page_level_ads: true,
+            });`,
+      }}
+    />
+  )
+  return [adsbygoogle, adsbygoogleConfig]
+}
